422680a53bd3783f5bcae922b12e9eee3da6a55e
Anarasty <murloc.ninja16@gmail.com>
2025-01-11 21:43:41 +0200

    improve header

---
+++ app/_data/Lookup.jsx
@@ -0,0 +1,6 @@
+export default {
+    HeroHeading:'AI Logo Maker',
+    HeroSubheading:'Perfect logos for apps, businesses and websites',
+    HeroDesc:'Create unique and professional logos effortlessly with our AI-powered tool. Perfect for apps, businesses, websites and more!',
+    InputTitlePlaceholder:'Enter your logo name',
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import { Divide } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
+      <Hero></Hero>
     </div>
   );
 }
