62adda3133b19d4714092c83d0cef634cd01db7b
Anarasty <murloc.ninja16@gmail.com>
2025-01-15 23:38:11 +0200

    testing apis

---
+++ app/create/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateLogo() {
+  return (
+    <div>
+      CreateLogo
+    </div>
+  )
+}
+
+export default CreateLogo
--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,6 @@ export default function RootLayout({ children }) {
         <Provider>
           {children}
         </Provider>
-        {children}
       </body>
     </html>
   );
