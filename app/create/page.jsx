5b048113bf61c42000c68d4c3f712f14101dcdc8
Anarasty <murloc.ninja16@gmail.com>
2025-01-21 17:37:38 +0200

    logo title page

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import LogoTitle from './_compenents/LogoTitle'
+import { Button } from '@/components/ui/button'
+import { ArrowLeft, ArrowRight } from 'lucide-react'
 
 function CreateLogo() {
   return (
-    <div>
-      CreateLogo
+    <div className='mt-28 p-10 border rounded-xl 2xl:mx-72'>
+      <LogoTitle/>
+
+      <div className='flex items-center justify-between'>
+        <Button variant="outline"> <ArrowLeft/> Previous</Button>
+        <Button> <ArrowRight/> Next</Button>
+      </div>
     </div>
   )
 }
